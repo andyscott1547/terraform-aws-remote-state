67bfe8ea7a2990d6d3e5ea7940cce14311fae5da
Andy Scott <andy.scott@bjss.com>
2022-10-24 09:03:38 +0100

    add region to s3 bucket name to ensure unique

--- locals.tf
+++ locals.tf
@@ -1,5 +1,5 @@
 # locals.terraform
 
 locals {
-  name = "${var.name}-${data.aws_caller_identity.current.account_id}"
+  name = "${var.name}-${data.aws_region.current.name}-${data.aws_caller_identity.current.account_id}"
 }
